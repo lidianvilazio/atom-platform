fd5c3913c129fdabdc678e0eb3ca358465ccb7a7
github username <github email address>
2018-10-02 15:27:05 -0400

    font

--- atom-platform-frontend/src/Components/AllLessons.js
+++ atom-platform-frontend/src/Components/AllLessons.js
@@ -10,27 +10,27 @@ class AllLessons extends React.Component {
 
   render() {
     let oldDate
-    const date = [].concat(this.props.data)
+    const date = [].concat(this.props.data)   // sorting by date returning a new array and mapping the data
     .sort((a, b) => a.time > b.time)
     .map((item, i) =>{
-      if(oldDate !== item.time.substring(0, 10)) {
+      if(oldDate !== item.time.substring(0, 10)) {   //  if oldDate is different of the element.time oldDate
+                                                     //  receive element.time and return it's data
         oldDate = item.time.substring(0, 10)
 
         return <div key={i}>
                   <Row>
                     <Col md={{ size: '12'}}>
-                    <h5 style={{width: '60%', marginBottom: '1%', marginTop: '2%'}}>
-                      <Moment format="dddd, MMMM DD, YYYY">{item.time}</Moment>
-                    </h5>
+                      <h5 style={{width: '60%', marginBottom: '1%', marginTop: '2%'}}>
+                        <Moment format="dddd, MMMM DD,YYYY">{item.time}</Moment>
+                      </h5>
                     </Col>
                   </Row>
                   <Lesson lesson={item}/>
                 </div>
-      } else{
+      } else{                                        // else returns the data
         return <Lesson key={i} lesson={item}/>}
       }
-    );
-
+    )
 
 
     return(
--- atom-platform-frontend/src/Components/Lesson.js
+++ atom-platform-frontend/src/Components/Lesson.js
@@ -4,77 +4,47 @@ import Moment from 'react-moment';
 import {  Row, Col } from 'reactstrap';
 import { Card, CardBody, CardText } from 'reactstrap';
 
-class Lesson extends React.Component {
-  render() {
-    console.log(this.props);
+const Lesson = (props) => {   //
+
+  const { lesson } = props;
+
     return(
       <div>
-      <Card>
-        <CardBody style={{color: '#fff', background: 'white', borderBottom: 'none'}}>
-        <Row>
-          <Col md="1" sm="6" xs="6">
-            <Avatar alt="subject Photo" src={this.props.lesson.subjectPhotoUrl} className='avatar'/>
-          </Col>
-          <Col md="6" sm="6" xs="6">
-            <h6 className='text'>{this.props.lesson.title}</h6>
-          </Col>
-          <Col md="1" sm="6" xs="6">
-            <Avatar alt="instructor Photo" src={this.props.lesson.instructorPhotoUrl} className='avatar_instructor'/>
-          </Col>
-          <Col md="2" sm="6" xs="6">
-            <p className='text'>{this.props.lesson.instructorName}</p>
-          </Col>
-          <Col md='2' sm='2'>
-            <p className='text'><Moment format="h.ms A">{this.props.lesson.time}</Moment></p>
-          </Col>
-        </Row>
-        </CardBody>
-        <CardBody>
-          <Row>
-            <Col md={{ size: '6', offset: 1}}>
-              <CardText className='f text'>{this.props.lesson.description}</CardText>
-            </Col>
-          </Row>
-        </CardBody>
-      </Card>
-    </div>
+        <Card>
+          <CardBody>
+            <Row>
+              <Col md="1" sm="6" xs="6">
+                <Avatar alt="subject Photo" src={lesson.subjectPhotoUrl} className='avatar'/>
+              </Col>
+              <Col md="6" sm="6" xs="6">
+                <h6 className='text title'>{lesson.title}</h6>
+              </Col>
+              <Col md="1" sm="6" xs="6">
+                <Avatar alt="instructor Photo" src={lesson.instructorPhotoUrl} className='avatar_instructor'/>
+              </Col>
+              <Col md="2" sm="6" xs="6">
+                <p className='text'>{lesson.instructorName}</p>
+              </Col>
+              <Col md='2' sm='2'>
+                <Row>
+                  <p className='text moment'><Moment format="h:ms A">{lesson.time}</Moment></p>
+                  <p>-</p>
+                  <p className='text moment'><Moment add={{ hours: 1 }} format="h:ms A">{lesson.time}</Moment> EDT</p>
+                </Row>
+              </Col>
+            </Row>
+          </CardBody>
+          <CardBody>
+            <Row>
+              <Col md={{ size: '6', offset: 1}}>
+                <CardText className='f text'>{lesson.description}</CardText>
+              </Col>
+            </Row>
+          </CardBody>
+       </Card>
+     </div>
+
     )
-  }
 }
 
 export default Lesson
-
-// <Row>
-// <Col>
-// <Card shadow={0}>
-// <Row>
-// <Col md="12" sm="12" xs="12">
-// <CardTitle style={{color: '#fff', background: 'white'}}>
-// <Col md="1" sm="6" xs="12">
-// <Avatar alt="subject Photo" src={this.props.lesson.subjectPhotoUrl} className='avatar'/>
-// </Col>
-// <Col md="6" sm="6" xs="12">
-// <p style={{color: 'black', fontSize: '10px'}}>{this.props.lesson.title}</p>
-// </Col>
-// <Col md="1" sm="6" xs="12">
-// <Avatar alt="instructor Photo" src={this.props.lesson.instructorPhotoUrl} className='avatar_instructor'/>
-// </Col>
-// <Col md="6" sm="6" xs="12">
-// <p style={{color: 'black',  fontSize: '10px'}}>{this.props.lesson.instructorName}</p>
-// </Col>
-// <Col>
-// <p style={{color: 'black',  fontSize: '10px'}}><Moment format="h.ms A">{this.props.lesson.time}</Moment></p>
-// </Col>
-// </CardTitle>
-// </Col>
-// </Row>
-// <Row>
-// <Col md={{ size: 6, offset: 1}} xs="12">
-// <CardText>
-// {this.props.lesson.description}
-// </CardText>
-// </Col>
-// </Row>
-// </Card>
-// </Col>
-// </Row>
